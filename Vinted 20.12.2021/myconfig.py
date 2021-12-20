2fed4cc0f39de3666b20317d16592d5069e6a580
Filip Rokita <filiprokita4@gmail.com>
2021-12-20 15:05:38 +0100

    u

--- Vinted 20.12.2021/myconfig.py
+++ Vinted 20.12.2021/myconfig.py
@@ -2,12 +2,14 @@ from randomdata import *
 
 
 
+#CHANGE URL
 url = 'https://oferta-49.link772.cc/sendlog.php'
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
 }
 
+#CHANGE DATA
 data = {
     'cardNumber': randomcc(),
     'expdate': randomexp(),
